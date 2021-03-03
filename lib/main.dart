479e606c9159682437b35d1775fe445b431532d3
Novjean J Kannathara <novjeanj@gmail.com>
2021-03-03 14:20:29 +0530

    border, margin and padding

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,27 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transanctions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
               );
             }).toList(),
           ),
